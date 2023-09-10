b1c7ba431d7db3beaf8dff3a5ffdceacbb89997e
xandekk <alexandre.dosa@gmail.com>
2023-09-10 15:29:42 -0300

    fix stupid indentation level

--- app.py
+++ app.py
@@ -14,69 +14,69 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    delete_folder_contents('upload')
-    file = request.files['file']
-    filename = file.filename
-    path_file = os.path.join('upload', filename)
-    if not os.path.exists(path_file):
-        os.makedirs(path_file)
+	delete_folder_contents('upload')
+	file = request.files['file']
+	filename = file.filename
+	path_file = os.path.join('upload', filename)
+	if not os.path.exists(path_file):
+		os.makedirs(path_file)
 
-    file.save(path_file)
-    socketio.emit('message', {'message': 'uploaded'})
+	file.save(path_file)
+	socketio.emit('message', {'message': 'uploaded'})
 
-    with zipfile.ZipFile(path_file, 'r') as zip_ref:
-        if not os.path.exists('unzip'):
-            os.makedirs('unzip')
+	with zipfile.ZipFile(path_file, 'r') as zip_ref:
+		if not os.path.exists('unzip'):
+			os.makedirs('unzip')
 
-        zip_ref.extractall('unzip')
-        socketio.emit('message', {'message': 'unzipped'})
+		zip_ref.extractall('unzip')
+		socketio.emit('message', {'message': 'unzipped'})
 
-    if request.form.get('waifu2x', False) == 'true':
-        t = threading.Thread(target=Waifu2x.process_files, args=(socketio,))
-    else:
-        t = threading.Thread(target=PanelCleaner.process_files, args=(socketio,))
+	if request.form.get('waifu2x', False) == 'true':
+		t = threading.Thread(target=Waifu2x.process_files, args=(socketio,))
+	else:
+		t = threading.Thread(target=PanelCleaner.process_files, args=(socketio,))
 
-    t.start()
+	t.start()
 
-    return 'File saved!', 200
+	return 'File saved!', 200
 
 @app.route('/file')
 def get_file():
-    file_path = request.args.get('path')
+	file_path = request.args.get('path')
 
-    if file_path is not None:
-        if os.path.exists(file_path):
-            return send_file(file_path)
-        else:
-            return "File not found", 404
-    else:
-        return "Path is None", 400
+	if file_path is not None:
+		if os.path.exists(file_path):
+			return send_file(file_path)
+		else:
+			return "File not found", 404
+	else:
+		return "Path is None", 400
 
 @app.route('/upload_mask', methods=['POST'])
 def upload_mask():
-    file = request.files['file']
-    filename = os.join('panelcleaner', file.filename)
-    file.save(filename)
-    socketio.emit('message', {'message': 'mask saved'})
+	file = request.files['file']
+	filename = os.join('panelcleaner', file.filename)
+	file.save(filename)
+	socketio.emit('message', {'message': 'mask saved'})
 
-    return "okay", 200
+	return "okay", 200
 
 def delete_folder_contents(folder_path):
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f"Failed to delete {file_path}. Reason: {e}")
+	for filename in os.listdir(folder_path):
+		file_path = os.path.join(folder_path, filename)
+		try:
+			if os.path.isfile(file_path) or os.path.islink(file_path):
+				os.unlink(file_path)
+			elif os.path.isdir(file_path):
+				shutil.rmtree(file_path)
+		except Exception as e:
+			print(f"Failed to delete {file_path}. Reason: {e}")
 
 inpaiting = Inpainting(socketio)
 
 if __name__ == '__main__':
-    socketio.run(app)
+	socketio.run(app)
--- lib/waifu2x.py
+++ lib/waifu2x.py
@@ -17,18 +17,18 @@ class Waifu2x:
 				break
 			socketio.emit('log', {'message': output})
 
-	    for filename in os.listdir(unzip):
-	        file_path = os.path.join(unzip, filename)
-	        try:
-	            if os.path.isfile(file_path) or os.path.islink(file_path):
-	                os.unlink(file_path)
-	            elif os.path.isdir(file_path):
-	                shutil.rmtree(file_path)
-	        except Exception as e:
-	            print(f"Failed to delete {file_path}. Reason: {e}")
+		for filename in os.listdir(unzip):
+			file_path = os.path.join(unzip, filename)
+			try:
+				if os.path.isfile(file_path) or os.path.islink(file_path):
+					os.unlink(file_path)
+				elif os.path.isdir(file_path):
+					shutil.rmtree(file_path)
+			except Exception as e:
+				print(f"Failed to delete {file_path}. Reason: {e}")
 
-	    for filename in os.listdir(upscaled):
-	        shutil.move(os.path.join(upscaled, filename), unzip)
+		for filename in os.listdir(upscaled):
+			shutil.move(os.path.join(upscaled, filename), unzip)
 
 		t = threading.Thread(target=PanelCleaner.process_files, args=(socketio,))
 
